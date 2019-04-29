898278c652cace760dc22b01ec93ffe37170536a
7030500574 <40025929+7030500574@users.noreply.github.com>
2019-04-29 20:45:16 +0530

    Add files via upload

---
+++ src/app/user/user-data.ts
@@ -0,0 +1,17 @@
+import { User } from './user';
+
+export const USER_PERSONS: User[] = [{
+    id: 1,
+    name: 'Nilesh',
+    Mobile:'7030500574'
+    
+  }, {
+    id: 2,
+    name: 'Amit',
+    Mobile:'7776865620'
+  }, {
+    id: 3,
+    name: 'Vishal',
+    Mobile:'9850964120'
+
+  }]
